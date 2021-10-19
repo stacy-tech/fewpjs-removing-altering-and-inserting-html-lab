4072b591af8c38a6f133316f807dab4da8a4eb7c
stacy <stacydn23@gmail.com>
2021-10-19 08:46:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// no longer has dom node 'main#main'
+document.querySelector('main#main').remove()
+
+// newHeader variable that points to node 'h1#victory'
+// created a variable newheader with a element/tagName of h1
+const newHeader = document.createElement('h1')
+
+// newheader variable with an id of victory
+// h1 element with an id of 'victory'
+newHeader.id  = 'victory'
+
+// newheader variable that points to the h1#victory with my name inside
+// an h1 with an id of victory with a text content with my name in there 
+newHeader.innerHTML = 'Stacy is the champion'
